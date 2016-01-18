1ef7fe103d621887e97d3e1754cc569d11a13f9c
Matthias Gattermeier <matthias.gattermeier@nytimes.com>
2016-01-18 13:09:51 -0500

    general structure

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class MorphModal extends React.Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+
+module.exports = MorphModal
